c7c376ecfa1def26f5d55c956a24ab14adf40145
kriffe <kristofer.skyttner@skymaker.se>
2016-05-14 11:05:11 +0200

    First import

---
+++ Assets/DemoCube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoCube : MonoBehaviour {
+
+	Vector3 diff = new Vector3(0,0,0);
+	Vector3 target = new Vector3(0,0,0);
+	Vector3 nextPos = new Vector3(0,0,0);
+
+	Vector3 startPos = new Vector3(0,0,0);
+	float dist = 10;
+	float speed = 1;
+	float dT = 0;
+
+	// Use this for initialization
+	void Start () {
+		startPos = this.transform.position;
+		dT = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dT = Time.time - dT;
+
+		target = startPos + new Vector3 (dist*Mathf.Sin (Time.time/1000), dist*Mathf.Sin(Time.time/1000), 0);
+
+		nextPos = target;
+		Debug.Log (nextPos);
+		this.transform.position = nextPos;
+
+	}
+}
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+	}
+
+	 
+	public void StartLevel(){
+		SceneManager.LoadScene (1);
+
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
