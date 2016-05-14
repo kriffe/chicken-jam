1b13bb0f67273437cfab3c0bc5e158a60c290b13
kriffe <kristofer.skyttner@skymaker.se>
2016-05-14 16:36:44 +0200

    GameManager updated

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,25 +9,37 @@ public class MenuScript : MonoBehaviour {
 	public Button exitText;
 
 
+
+
 	// Use this for initialization
 	void Start () {
-
 		startText = startText.GetComponent<Button> ();
 		exitText = exitText.GetComponent<Button> ();
 	}
 
 	 
 	public void StartLevel(){
-		SceneManager.LoadScene (1);
-
+		startText.enabled = false;
+		GameManager.instance.startNextLevel ();
 	}
 
 	public void ExitGame(){
 		Application.Quit ();
 	}
+
+	public void hide(){
+		
+		exitText.enabled = false;
+	}
+
+	public void show(){
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
