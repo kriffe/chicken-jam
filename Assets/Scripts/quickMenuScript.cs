9aeafd8def7e8d3038656308451a23e7084d0b1f
kriffe <kristofer.skyttner@skymaker.se>
2016-05-14 17:22:57 +0200

    Qucik Menu added

---
+++ Assets/Scripts/quickMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class quickMenuScript : MonoBehaviour {
+
+	public Canvas quickMenu;
+	public Button startText;
+
+	// Use this for initialization
+	void Start () {
+		quickMenu = quickMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+	}
+
+
+	void StartApp(){
+		GameManager.instance.startNextLevel ();
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
